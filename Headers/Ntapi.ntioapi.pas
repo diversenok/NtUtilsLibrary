efc0954e60565ae99c6f1dc5552f68b14389fb1a
diversenok <diversenok@gmail.com>
2019-09-17 17:54:28 +0200

    Add file operations api
     - Create, open, rename
     - Query/set information
     - Create and enumerate hardlinks
     - Enumerate streams

--- Headers/Ntapi.ntioapi.pas
+++ Headers/Ntapi.ntioapi.pas
@@ -5,7 +5,7 @@ unit Ntapi.ntioapi;
 interface
 
 uses
-  Winapi.WinNt;
+  Winapi.WinNt, Ntapi.ntdef;
 
 const
   // WinNt.12936
@@ -30,6 +30,294 @@ const
   FILE_SHARE_READ = $00000001;
   FILE_SHARE_WRITE = $00000002;
   FILE_SHARE_DELETE = $00000004;
+  FILE_SHARE_ALL = FILE_SHARE_READ or FILE_SHARE_WRITE or FILE_SHARE_DELETE;
+
+  FILE_ATTRIBUTE_READONLY = $00000001;
+  FILE_ATTRIBUTE_HIDDEN = $00000002;
+  FILE_ATTRIBUTE_SYSTEM = $00000004;
+  FILE_ATTRIBUTE_DIRECTORY = $00000010;
+  FILE_ATTRIBUTE_ARCHIVE = $00000020;
+  FILE_ATTRIBUTE_DEVICE = $00000040;
+  FILE_ATTRIBUTE_NORMAL = $00000080;
+  FILE_ATTRIBUTE_TEMPORARY = $00000100;
+  FILE_ATTRIBUTE_SPARSE_FILE = $00000200;
+  FILE_ATTRIBUTE_REPARSE_POINT = $00000400;
+  FILE_ATTRIBUTE_COMPRESSED = $00000800;
+  FILE_ATTRIBUTE_OFFLINE = $00001000;
+  FILE_ATTRIBUTE_NOT_CONTENT_INDEXED = $00002000;
+  FILE_ATTRIBUTE_ENCRYPTED = $00004000;
+  FILE_ATTRIBUTE_INTEGRITY_STREAM = $00008000;
+  FILE_ATTRIBUTE_VIRTUAL = $00010000;
+  FILE_ATTRIBUTE_NO_SCRUB_DATA = $00020000;
+  FILE_ATTRIBUTE_EA = $00040000;
+  FILE_ATTRIBUTE_PINNED = $00080000;
+  FILE_ATTRIBUTE_UNPINNED = $00100000;
+  FILE_ATTRIBUTE_RECALL_ON_OPEN = $00040000;
+  FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS = $00400000;
+
+  // Create disposition
+  FILE_SUPERSEDE = $00000000;
+  FILE_OPEN = $00000001;
+  FILE_CREATE = $00000002;
+  FILE_OPEN_IF = $00000003;
+  FILE_OVERWRITE = $00000004;
+  FILE_OVERWRITE_IF = $00000005;
+  FILE_MAXIMUM_DISPOSITION = $00000005;
+
+  // Create/open flags
+  FILE_DIRECTORY_FILE = $00000001;
+  FILE_WRITE_THROUGH = $00000002;
+  FILE_SEQUENTIAL_ONLY = $00000004;
+  FILE_NO_INTERMEDIATE_BUFFERING = $00000008;
+  FILE_SYNCHRONOUS_IO_ALERT = $00000010;
+  FILE_SYNCHRONOUS_IO_NONALERT = $00000020;
+  FILE_NON_DIRECTORY_FILE = $00000040;
+  FILE_CREATE_TREE_CONNECTION = $00000080;
+  FILE_COMPLETE_IF_OPLOCKED = $00000100;
+  FILE_NO_EA_KNOWLEDGE = $00000200;
+  FILE_OPEN_FOR_RECOVERY = $00000400;
+  FILE_RANDOM_ACCESS = $00000800;
+  FILE_DELETE_ON_CLOSE = $00001000;
+  FILE_OPEN_BY_FILE_ID = $00002000;
+  FILE_OPEN_FOR_BACKUP_INTENT = $00004000;
+  FILE_NO_COMPRESSION = $00008000;
+  FILE_OPEN_REQUIRING_OPLOCK = $00010000;
+  FILE_DISALLOW_EXCLUSIVE = $00020000;
+  FILE_SESSION_AWARE = $00040000;
+
+  // IO status results
+  FILE_SUPERSEDED = $00000000;
+  FILE_OPENED = $00000001;
+  FILE_CREATED = $00000002;
+  FILE_OVERWRITTEN = $00000003;
+  FILE_EXISTS = $00000004;
+  FILE_DOES_NOT_EXIST = $00000005;
+
+type
+  TIoStatusBlock = record
+    Status: NTSTATUS;
+    {$IFDEF WIN64}
+    Padding: Cardinal;
+    {$ENDIF}
+    Information: NativeUInt;
+  end;
+  PIoStatusBlock = ^TIoStatusBlock;
+
+  TFileInformationClass = (
+    FileReserved = 0,
+    FileDirectoryInformation = 1,     //
+    FileFullDirectoryInformation = 2, //
+    FileBothDirectoryInformation = 3, //
+    FileBasicInformation = 4,         // q, s: TFileBasicInformation
+    FileStandardInformation = 5,      // q: TFileStandardInformation
+    FileInternalInformation = 6,      // q:
+    FileEaInformation = 7,            // q: Cardinal (Size)
+    FileAccessInformation = 8,        // q: TAccessMask
+    FileNameInformation = 9,          // q: TFileNameInformation
+    FileRenameInformation = 10,       // s: TFileRenameInformation
+    FileLinkInformation = 11,         // s: TFileLinkInformation
+    FileNamesInformation = 12,        // q:
+    FileDispositionInformation = 13,  // s: Boolean (Delete file)
+    FilePositionInformation = 14,     // q, s: UInt64 (Byte offset)
+    FileFullEaInformation = 15,       // q:
+    FileModeInformation = 16,         // q, s: Cardinal
+    FileAlignmentInformation = 17,    // q: Cardinal
+    FileAllInformation = 18,          // q:
+    FileAllocationInformation = 19,   // s: UInt64 (Size)
+    FileEndOfFileInformation = 20,    // s: UInt64
+    FileAlternateNameInformation = 21,// q:
+    FileStreamInformation = 22,       // q: TFileStreamInformation
+    FilePipeInformation = 23,         // q, s:
+    FilePipeLocalInformation = 24,    // q:
+    FilePipeRemoteInformation = 25,   // q, s:
+    FileMailslotQueryInformation = 26,// q:
+    FileMailslotSetInformation = 27,  // s:
+    FileCompressionInformation = 28,  // q: TFileCompressionInformation
+    FileObjectIdInformation = 29,     // q, s:
+    FileCompletionInformation = 30,   // s:
+    FileMoveClusterInformation = 31,  // s:
+    FileQuotaInformation = 32,        // q, s:
+    FileReparsePointInformation = 33, // q: TFileReparsePointInformation
+    FileNetworkOpenInformation = 34,  // q:
+    FileAttributeTagInformation = 35, // q: TFileAttributeTagInformation
+    FileTrackingInformation = 36,     // s:
+    FileIdBothDirectoryInformation = 37, //
+    FileIdFullDirectoryInformation = 38, //
+    FileValidDataLengthInformation = 39, // s: UInt64
+    FileShortNameInformation = 40,       // s:
+    FileIoCompletionNotificationInformation = 41, // q, s:
+    FileIoStatusBlockRangeInformation = 42, // s:
+    FileIoPriorityHintInformation = 43,  // q, s:
+    FileSfioReserveInformation = 44,     // q, s:
+    FileSfioVolumeInformation = 45,      // q:
+    FileHardLinkInformation = 46         // q: TFileLinksInformation
+  );
+
+  // FileBasicInformation
+  TFileBasicInformation = record
+    CreationTime: TLargeInteger;
+    LastAccessTime: TLargeInteger;
+    LastWriteTime: TLargeInteger;
+    ChangeTime: TLargeInteger;
+    FileAttributes: Cardinal;
+  end;
+  PFileBasicInformation = ^TFileBasicInformation;
+
+  // FileStandardInformation
+  TFileStandardInformation = record
+    AllocationSize: UInt64;
+    EndOfFile: UInt64;
+    NumberOfLinks: Cardinal;
+    DeletePending: Boolean;
+    Directory: Boolean;
+  end;
+  PFileStandardInformation = ^TFileStandardInformation;
+
+  // FileNameInformation
+  TFileNameInformation = record
+    FileNameLength: Cardinal;
+    FileName: array [ANYSIZE_ARRAY] of WideChar;
+  end;
+  PFileNameInformation = ^TFileNameInformation;
+
+  // FileRenameInformation
+  TFileRenameInformation = record
+    ReplaceIfExists: Boolean;
+    RootDirectory: THandle;
+    FileNameLength: Cardinal;
+    FileName: array [ANYSIZE_ARRAY] of WideChar;
+  end;
+  PFileRenameInformation = ^TFileRenameInformation;
+
+  // FileLinkInformation
+  TFileLinkInformation = TFileRenameInformation;
+  PFileLinkInformation = ^TFileLinkInformation;
+
+  // FileStreamInformation
+  TFileStreamInformation = record
+    NextEntryOffset: Cardinal;
+    StreamNameLength: Cardinal;
+    StreamSize: UInt64;
+    StreamAllocationSize: UInt64;
+    StreamName: array [ANYSIZE_ARRAY] of WideChar;
+  end;
+  PFileStreamInformation = ^TFileStreamInformation;
+
+  // FileCompressionInformation
+  TFileCompressionInformation = record
+    CompressedFileSize: UInt64;
+    CompressionFormat: Word;
+    CompressionUnitShift: Byte;
+    ChunkShift: Byte;
+    ClusterShift: Byte;
+    Reserved: array [0..2] of Byte;
+  end;
+  PFileCompressionInformation = ^TFileCompressionInformation;
+
+  // FileReparsePointInformation
+  TFileReparsePointInformation = record
+    FileReference: Int64;
+    Tag: Cardinal;
+  end;
+  PFileReparsePointInformation = ^TFileReparsePointInformation;
+
+  // FileAttributeTagInformation
+  TFileAttributeTagInformation = record
+    FileAttributes: Cardinal;
+    ReparseTag: Cardinal;
+  end;
+  PFileAttributeTagInformation = ^TFileAttributeTagInformation;
+
+  TFileLinkEntryInformation = record
+    NextEntryOffset: Cardinal;
+    ParentFileId: Int64;
+    FileNameLength: Cardinal;
+    FileName: array [ANYSIZE_ARRAY] of WideChar;
+  end;
+  PFileLinkEntryInformation = ^TFileLinkEntryInformation;
+
+  // FileHardLinkInformation
+  TFileLinksInformation = record
+    BytesNeeded: Cardinal;
+    EntriesReturned: Cardinal;
+    Entry: TFileLinkEntryInformation;
+  end;
+  PFileLinksInformation = ^TFileLinksInformation;
+
+  TFsInfoClass = (
+    FileFsReserved = 0,
+    FileFsVolumeInformation = 1,      // q: TFileFsVolumeInformation
+    FileFsLabelInformation = 2,       // s: TFileFsLabelInformation
+    FileFsSizeInformation = 3,        // q: TFileFsSizeInformation
+    FileFsDeviceInformation = 4,      // q:
+    FileFsAttributeInformation = 5,   // q:
+    FileFsControlInformation = 6,     // q, s:
+    FileFsFullSizeInformation = 7,    // q:
+    FileFsObjectIdInformation = 8,    // q, s:
+    FileFsDriverPathInformation = 9,  // q:
+    FileFsVolumeFlagsInformation = 10 // q, s: Cardinal
+  );
+
+  // FileFsVolumeInformation
+  TFileFsVolumeInformation = record
+    VolumeCreationTime: TLargeInteger;
+    VolumeSerialNumber: Cardinal;
+    VolumeLabelLength: Cardinal;
+    SupportsObjects: Boolean;
+    VolumeLabel: array [ANYSIZE_ARRAY] of WideChar;
+  end;
+  PFileFsVolumeInformation = ^TFileFsVolumeInformation;
+
+  // FileFsLabelInformation
+  TFileFsLabelInformation = record
+    VolumeLabelLength: Cardinal;
+    VolumeLabel: array [ANYSIZE_ARRAY] of WideChar;
+  end;
+  PFileFsLabelInformation = ^TFileFsLabelInformation;
+
+  // FileFsSizeInformation
+  TFileFsSizeInformation = record
+    TotalAllocationUnits: UInt64;
+    AvailableAllocationUnits: UInt64;
+    SectorsPerAllocationUnit: Cardinal;
+    BytesPerSector: Cardinal;
+  end;
+  PFileFsSizeInformation = ^TFileFsSizeInformation;
+
+function NtCreateFile(out FileHandle: THandle; DesiredAccess: TAccessMask;
+  const ObjectAttributes: TObjectAttributes; out IoStatusBlock: TIoStatusBlock;
+  AllocationSize: PLargeInteger; FileAttributes: Cardinal; ShareAccess:
+  Cardinal; CreateDisposition: Cardinal; CreateOptions: Cardinal;
+  EaBuffer: Pointer; EaLength: Cardinal): NTSTATUS; stdcall; external ntdll;
+
+function NtOpenFile(out FileHandle: THandle; DesiredAccess: TAccessMask;
+  const ObjectAttributes: TObjectAttributes; out IoStatusBlock: TIoStatusBlock;
+  ShareAccess: Cardinal; OpenOptions: Cardinal): NTSTATUS; stdcall;
+  external ntdll;
+
+function NtDeleteFile(const ObjectAttributes: TObjectAttributes): NTSTATUS;
+  stdcall; external ntdll;
+
+function NtFlushBuffersFile(FileHandle: THandle; out IoStatusBlock:
+  TIoStatusBlock): NTSTATUS; stdcall; external ntdll;
+
+function NtQueryInformationFile(FileHandle: THandle; out IoStatusBlock:
+  TIoStatusBlock; FileInformation: Pointer; Length: Cardinal;
+  FileInformationClass: TFileInformationClass): NTSTATUS; stdcall;
+  external ntdll;
+
+function NtSetInformationFile(FileHandle: THandle; out IoStatusBlock:
+  TIoStatusBlock; FileInformation: Pointer; Length: Cardinal;
+  FileInformationClass: TFileInformationClass): NTSTATUS; stdcall;
+  external ntdll;
+
+function NtQueryVolumeInformationFile(FileHandle: THandle; out IoStatusBlock:
+  TIoStatusBlock; FsInformation: Pointer; Length: Cardinal; FsInformationClass:
+  TFsInfoClass): NTSTATUS; stdcall; external ntdll;
+
+function NtSetVolumeInformationFile(FileHandle: THandle; out IoStatusBlock:
+  TIoStatusBlock; FsInformation: Pointer; Length: Cardinal; FsInformationClass:
+  TFsInfoClass): NTSTATUS; stdcall; external ntdll;
 
 implementation
 
---
+++ NtUtils.Files.pas
@@ -0,0 +1,427 @@
+unit NtUtils.Files;
+
+interface
+
+uses
+  Winapi.WinNt, Ntapi.ntioapi, NtUtils.Exceptions;
+
+type
+  TFileStreamInfo = record
+    StreamSize: Int64;
+    StreamAllocationSize: Int64;
+    StreamName: String;
+  end;
+
+  TFileHardlinkLinkInfo = record
+    ParentFileId: Int64;
+    FileName: String;
+  end;
+
+{ Open & Create }
+
+// Create/open a file
+function NtxCreateFile(out hFile: THandle; DesiredAccess: THandle;
+  FileName: String; Root: THandle = 0; CreateDisposition: Cardinal =
+  FILE_CREATE; ShareAccess: Cardinal = FILE_SHARE_ALL; CreateOptions:
+  Cardinal = 0; FileAttributes: Cardinal = FILE_ATTRIBUTE_NORMAL;
+  HandleAttributes: Cardinal = 0; ActionTaken: PCardinal = nil): TNtxStatus;
+
+// Open a file
+function NtxOpenFile(out hFile: THandle; DesiredAccess: TAccessMask;
+  FileName: String; Root: THandle = 0; ShareAccess: THandle = FILE_SHARE_ALL;
+  OpenOptions: Cardinal = 0; HandleAttributes: Cardinal = 0): TNtxStatus;
+
+// Open a file by ID
+function NtxOpenFileById(out hFile: THandle; DesiredAccess: TAccessMask;
+  FileId: Int64; Root: THandle = 0; ShareAccess: THandle = FILE_SHARE_READ or
+  FILE_SHARE_WRITE or FILE_SHARE_DELETE; HandleAttributes: Cardinal = 0)
+  : TNtxStatus;
+
+{ Operations }
+
+// Rename a file
+function NtxRenameFile(hFile: THandle; NewName: String;
+  ReplaceIfExists: Boolean = False; RootDirectory: THandle = 0): TNtxStatus;
+
+// Creare a hardlink for a file
+function NtxHardlinkFile(hFile: THandle; NewName: String;
+  ReplaceIfExists: Boolean = False; RootDirectory: THandle = 0): TNtxStatus;
+
+{ Information }
+
+// Query variable-length information
+function NtxQueryFile(hFile: THandle; InfoClass: TFileInformationClass;
+  out Status: TNtxStatus; InitialBufferSize: Cardinal; ReturedLength:
+  PCardinal = nil): Pointer;
+
+// Set variable-length information
+function NtxSetFile(hFile: THandle; InfoClass: TFileInformationClass;
+  Buffer: Pointer; BufferSize: Cardinal): TNtxStatus;
+
+type
+  NtxFile = class
+    // Query fixed-size information
+    class function Query<T>(hFile: THandle;
+      InfoClass: TFileInformationClass; out Buffer: T): TNtxStatus; static;
+
+    // Set fixed-size information
+    class function SetInfo<T>(hFile: THandle;
+      InfoClass: TFileInformationClass; const Buffer: T): TNtxStatus; static;
+  end;
+
+// Query name of a file
+function NtxQueryNameFile(hFile: THandle; out Name: String): TNtxStatus;
+
+// Enumerate file streams
+function NtxEnumerateStreamsFile(hFile: THandle; out Streams:
+  TArray<TFileStreamInfo>) : TNtxStatus;
+
+// Enumerate hardlinks pointing to the file
+function NtxEnumerateHardLinksFile(hFile: THandle; out Links:
+  TArray<TFileHardlinkLinkInfo>) : TNtxStatus;
+
+// Get full name of a hardlink target
+function NtxExpandHardlinkTarget(hOriginalFile: THandle;
+  const Hardlink: TFileHardlinkLinkInfo; out FullName: String): TNtxStatus;
+
+implementation
+
+uses
+  Ntapi.ntdef, Ntapi.ntstatus, NtUtils.Objects;
+
+{ Open & Create }
+
+function NtxCreateFile(out hFile: THandle; DesiredAccess: THandle;
+  FileName: String; Root: THandle; CreateDisposition: Cardinal;
+  ShareAccess: Cardinal; CreateOptions: Cardinal; FileAttributes: Cardinal;
+  HandleAttributes: Cardinal; ActionTaken: PCardinal): TNtxStatus;
+var
+  ObjAttr: TObjectAttributes;
+  ObjName: UNICODE_STRING;
+  IoStatusBlock: TIoStatusBlock;
+begin
+  ObjName.FromString(FileName);
+  InitializeObjectAttributes(ObjAttr, @ObjName, HandleAttributes, Root);
+
+  Result.Location := 'NtCreateFile';
+  Result.LastCall.CallType := lcOpenCall;
+  Result.LastCall.AccessMask := DesiredAccess;
+  Result.LastCall.AccessMaskType := objIoFile;
+
+  Result.Status := NtCreateFile(hFile, DesiredAccess, ObjAttr, IoStatusBlock,
+    nil, FileAttributes, ShareAccess, CreateDisposition, CreateOptions, nil, 0);
+
+  if Result.IsSuccess and Assigned(ActionTaken) then
+    ActionTaken^ := Cardinal(IoStatusBlock.Information);
+end;
+
+function NtxOpenFile(out hFile: THandle; DesiredAccess: TAccessMask;
+  FileName: String; Root: THandle; ShareAccess: THandle; OpenOptions: Cardinal;
+  HandleAttributes: Cardinal): TNtxStatus;
+var
+  ObjName: UNICODE_STRING;
+  ObjAttr: TObjectAttributes;
+  IoStatusBlock: TIoStatusBlock;
+begin
+  ObjName.FromString(FileName);
+  InitializeObjectAttributes(ObjAttr, @ObjName, HandleAttributes, Root);
+
+  Result.Location := 'NtOpenFile';
+  Result.LastCall.CallType := lcOpenCall;
+  Result.LastCall.AccessMask := DesiredAccess;
+  Result.LastCall.AccessMaskType := objIoFile;
+
+  Result.Status := NtOpenFile(hFile, DesiredAccess, ObjAttr, IoStatusBlock,
+    ShareAccess, OpenOptions);
+end;
+
+function NtxOpenFileById(out hFile: THandle; DesiredAccess: TAccessMask;
+  FileId: Int64; Root: THandle; ShareAccess: THandle; HandleAttributes:
+  Cardinal): TNtxStatus;
+var
+  ObjName: UNICODE_STRING;
+  ObjAttr: TObjectAttributes;
+  IoStatusBlock: TIoStatusBlock;
+begin
+  ObjName.Length := SizeOf(FileId);
+  ObjName.MaximumLength := SizeOf(FileId);
+  ObjName.Buffer := PWideChar(@FileId); // Pass binary value
+
+  InitializeObjectAttributes(ObjAttr, @ObjName, HandleAttributes, Root);
+
+  Result.Location := 'NtOpenFile';
+  Result.LastCall.CallType := lcOpenCall;
+  Result.LastCall.AccessMask := DesiredAccess;
+  Result.LastCall.AccessMaskType := objIoFile;
+
+  Result.Status := NtOpenFile(hFile, DesiredAccess, ObjAttr, IoStatusBlock,
+    ShareAccess, FILE_OPEN_BY_FILE_ID or FILE_SYNCHRONOUS_IO_NONALERT);
+end;
+
+{ Operations }
+
+function NtxpSetRenameInfoFile(hFile: THandle; TargetName: String;
+  ReplaceIfExists: Boolean; RootDirectory: THandle;
+  InfoClass: TFileInformationClass): TNtxStatus;
+var
+  Buffer: PFileRenameInformation; // aka PFileLinkInformation
+  BufferSize: Cardinal;
+begin
+  // Prepare a variable-length buffer for rename or hardlink operation
+  BufferSize := SizeOf(TFileRenameInformation) +
+    Length(TargetName) * SizeOf(WideChar);
+  Buffer := AllocMem(BufferSize);
+
+  Buffer.ReplaceIfExists := ReplaceIfExists;
+  Buffer.RootDirectory := RootDirectory;
+  Buffer.FileNameLength := Length(TargetName) * SizeOf(WideChar);
+  Move(PWideChar(TargetName)^, Buffer.FileName, Buffer.FileNameLength);
+
+  Result := NtxSetFile(hFile, InfoClass, Buffer, BufferSize);
+  FreeMem(Buffer);
+end;
+
+function NtxRenameFile(hFile: THandle; NewName: String;
+  ReplaceIfExists: Boolean; RootDirectory: THandle): TNtxStatus;
+begin
+  // Note: if you get sharing violation when using RootDirectory open it with
+  // FILE_TRAVERSE | FILE_READ_ATTRIBUTES access.
+
+  Result := NtxpSetRenameInfoFile(hFile, NewName, ReplaceIfExists,
+    RootDirectory, FileRenameInformation);
+end;
+
+function NtxHardlinkFile(hFile: THandle; NewName: String;
+  ReplaceIfExists: Boolean; RootDirectory: THandle): TNtxStatus;
+begin
+  Result := NtxpSetRenameInfoFile(hFile, NewName, ReplaceIfExists,
+    RootDirectory, FileLinkInformation);
+end;
+
+{ Information }
+
+procedure NtxpFormatFileQuery(var Status: TNtxStatus;
+  InfoClass: TFileInformationClass);
+begin
+  Status.Location := 'NtQueryInformationFile';
+  Status.LastCall.CallType := lcQuerySetCall;
+  Status.LastCall.InfoClass := Cardinal(InfoClass);
+  Status.LastCall.InfoClassType := TypeInfo(TFileInformationClass);
+end;
+
+function NtxQueryFile(hFile: THandle; InfoClass: TFileInformationClass;
+  out Status: TNtxStatus; InitialBufferSize: Cardinal; ReturedLength: PCardinal)
+  : Pointer;
+var
+  IoStatusBlock: TIoStatusBlock;
+  BufferSize: Cardinal;
+begin
+  NtxpFormatFileQuery(Status, InfoClass);
+
+  BufferSize := InitialBufferSize;
+  repeat
+    Result := AllocMem(BufferSize);
+
+    IoStatusBlock.Information := 0;
+    Status.Status := NtQueryInformationFile(hFile, IoStatusBlock, Result,
+      BufferSize, InfoClass);
+
+    if not Status.IsSuccess then
+    begin
+      FreeMem(Result);
+      Result := nil;
+    end;
+  until not NtxExpandBuffer(Status, BufferSize, BufferSize shl 1 + 256);
+
+  if Assigned(ReturedLength) then
+    ReturedLength^ := Cardinal(IoStatusBlock.Information);
+end;
+
+function NtxSetFile(hFile: THandle; InfoClass: TFileInformationClass;
+  Buffer: Pointer; BufferSize: Cardinal): TNtxStatus;
+var
+  IoStatusBlock: TIoStatusBlock;
+begin
+  Result.Location := 'NtSetInformationFile';
+  Result.LastCall.CallType := lcQuerySetCall;
+  Result.LastCall.InfoClass := Cardinal(InfoClass);
+  Result.LastCall.InfoClassType := TypeInfo(TFileInformationClass);
+
+  Result.Status := NtSetInformationFile(hFile, IoStatusBlock, Buffer,
+    BufferSize, InfoClass);
+end;
+
+class function NtxFile.Query<T>(hFile: THandle;
+  InfoClass: TFileInformationClass; out Buffer: T): TNtxStatus;
+var
+  IoStatusBlock: TIoStatusBlock;
+begin
+  Result.Location := 'NtQueryInformationFile';
+  Result.LastCall.CallType := lcQuerySetCall;
+  Result.LastCall.InfoClass := Cardinal(InfoClass);
+  Result.LastCall.InfoClassType := TypeInfo(TFileInformationClass);
+
+  Result.Status := NtQueryInformationFile(hFile, IoStatusBlock, @Buffer,
+    SizeOf(Buffer), InfoClass);
+end;
+
+class function NtxFile.SetInfo<T>(hFile: THandle;
+  InfoClass: TFileInformationClass; const Buffer: T): TNtxStatus;
+begin
+  Result := NtxSetFile(hFile, InfoClass, @Buffer, SizeOf(Buffer));
+end;
+
+function NtxQueryNameFile(hFile: THandle; out Name: String): TNtxStatus;
+var
+  Buffer: PFileNameInformation;
+  BufferSize, Required: Cardinal;
+  IoStatusBlock: TIoStatusBlock;
+begin
+  NtxpFormatFileQuery(Result, FileNameInformation);
+
+  BufferSize := SizeOf(TFileNameInformation);
+  repeat
+    Buffer := AllocMem(BufferSize);
+
+    Result.Status := NtQueryInformationFile(hFile, IoStatusBlock, Buffer,
+      BufferSize, FileNameInformation);
+
+    Required := SizeOf(Cardinal) + Buffer.FileNameLength;
+
+    if not Result.IsSuccess then
+    begin
+      FreeMem(Buffer);
+      Buffer := nil;
+    end;
+  until not NtxExpandBuffer(Result, BufferSize, Required);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  SetString(Name, Buffer.FileName, Buffer.FileNameLength div 2);
+  FreeMem(Buffer);
+end;
+
+function NtxEnumerateStreamsFile(hFile: THandle; out Streams:
+  TArray<TFileStreamInfo>) : TNtxStatus;
+var
+  Buffer, pStream: PFileStreamInformation;
+  BufferSize, Required: Cardinal;
+  IoStatusBlock: TIoStatusBlock;
+begin
+  NtxpFormatFileQuery(Result, FileStreamInformation);
+
+  BufferSize := SizeOf(TFileStreamInformation);
+  repeat
+    Buffer := AllocMem(BufferSize);
+
+    Result.Status := NtQueryInformationFile(hFile, IoStatusBlock, Buffer,
+      BufferSize, FileStreamInformation);
+
+    Required := BufferSize shl 1 + 64;
+
+    if not Result.IsSuccess then
+    begin
+      FreeMem(Buffer);
+      Buffer := nil;
+    end;
+  until not NtxExpandBuffer(Result, BufferSize, Required);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  SetLength(Streams, 0);
+  pStream := Buffer;
+
+  repeat
+    SetLength(Streams, Length(Streams) + 1);
+    Streams[High(Streams)].StreamSize := pStream.StreamSize;
+    Streams[High(Streams)].StreamAllocationSize := pStream.StreamAllocationSize;
+    SetString(Streams[High(Streams)].StreamName, pStream.StreamName,
+      pStream.StreamNameLength div 2);
+
+    if pStream.NextEntryOffset <> 0 then
+      pStream := Pointer(NativeUInt(pStream) + pStream.NextEntryOffset)
+    else
+      Break;
+
+  until False;
+
+  FreeMem(Buffer);
+end;
+
+function NtxEnumerateHardLinksFile(hFile: THandle; out Links:
+  TArray<TFileHardlinkLinkInfo>) : TNtxStatus;
+var
+  Buffer: PFileLinksInformation;
+  pLink: PFileLinkEntryInformation;
+  BufferSize, Required: Cardinal;
+  IoStatusBlock: TIoStatusBlock;
+  i: Integer;
+begin
+  NtxpFormatFileQuery(Result, FileHardLinkInformation);
+
+  BufferSize := SizeOf(TFileLinksInformation);
+  repeat
+    Buffer := AllocMem(BufferSize);
+
+    Result.Status := NtQueryInformationFile(hFile, IoStatusBlock, Buffer,
+      BufferSize, FileHardLinkInformation);
+
+    Required := Buffer.BytesNeeded;
+
+    if not Result.IsSuccess then
+    begin
+      FreeMem(Buffer);
+      Buffer := nil;
+    end;
+  until not NtxExpandBuffer(Result, BufferSize, Required);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  SetLength(Links, Buffer.EntriesReturned);
+
+  pLink := @Buffer.Entry;
+  i := 0;
+
+  repeat
+    if i > High(Links) then
+      Break;
+
+    // Note: we have only the filename and the ID of the parent directory
+
+    Links[i].ParentFileId := pLink.ParentFileId;
+    SetString(Links[i].FileName, pLink.FileName, pLink.FileNameLength);
+
+    if pLink.NextEntryOffset <> 0 then
+      pLink := Pointer(NativeUInt(pLink) + pLink.NextEntryOffset)
+    else
+      Break;
+
+    Inc(i);
+  until False;
+
+  FreeMem(Buffer);
+end;
+
+function NtxExpandHardlinkTarget(hOriginalFile: THandle;
+  const Hardlink: TFileHardlinkLinkInfo; out FullName: String): TNtxStatus;
+var
+  hFile: THandle;
+begin
+  Result := NtxOpenFileById(hFile, SYNCHRONIZE or FILE_READ_ATTRIBUTES,
+    Hardlink.ParentFileId, hOriginalFile);
+
+  if Result.IsSuccess then
+  begin
+    Result := NtxQueryNameFile(hFile, FullName);
+
+    if Result.IsSuccess then
+      FullName := FullName + '\' + Hardlink.FileName;
+
+    NtxSafeClose(hFile);
+  end;
+end;
+
+end.
