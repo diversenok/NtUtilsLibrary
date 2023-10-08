f6a39b6bdbc47acdf9299d740d5a19b8ca9c937d
diversenok <diversenok@gmail.com>
2023-10-08 23:26:59 +0200

    Add reflectionless and always-interactive status dialogs

---
+++ NtUiLib/NtUiLib.Errors.Dialog.pas
@@ -0,0 +1,105 @@
+unit NtUiLib.Errors.Dialog;
+
+interface
+
+uses
+  Ntapi.WinUser, NtUtils;
+
+const
+  DEFAULT_CROSS_SESSION_MESSAGE_TIMEOUT = 60; // sec
+
+// Show a modal error message dialog
+function ShowNtxStatus(
+  ParentWnd: THwnd;
+  const Status: TNtxStatus
+): TNtxStatus;
+
+// Show a error message dialog to the interactive user
+function ShowNtxStatusAlwaysInteractive(
+  const Status: TNtxStatus;
+  TimeoutSeconds: Cardinal = DEFAULT_CROSS_SESSION_MESSAGE_TIMEOUT
+): TNtxStatus;
+
+// Format a status message (without using reflection)
+function NtxVerboseStatusMessageNoReflection(
+  const Status: TNtxStatus
+): String;
+
+var
+  // A custom status formatter (provided by NtUiLib.Exceptions.Dialog)
+  NtxVerboseStatusMessageFormatter: function (const Status: TNtxStatus): String;
+
+implementation
+
+uses
+  Ntapi.ntdef, NtUtils.SysUtils, NtUiLib.Errors, NtUiLib.TaskDialog;
+
+function NtxVerboseStatusMessageNoReflection;
+begin
+  // LastCall: <function name>
+  Result := 'Last call: ' + RtlxStringOrDefault(Status.Location, '<unknown>');
+
+  if Status.LastCall.Parameter <> '' then
+    Result := Result +  #$D#$A'Parameter: ' + Status.LastCall.Parameter;
+
+  // Result: <STATUS_*/ERROR_*>
+  Result := Result + #$D#$A'Result: ' + Status.Name;
+
+  // <textual description>
+  Result := Result + #$D#$A#$D#$A + Status.Description;
+end;
+
+procedure RtlxpPrepareStatusMessage(
+  const Status: TNtxStatus;
+  out Icon: TDialogIcon;
+  out Title: String;
+  out Summary: String;
+  out Content: String
+);
+begin
+  if Status.IsHResult or NT_ERROR(Status.Status) then
+  begin
+    Icon := diError;
+    Title := 'Error';
+  end
+  else
+  begin
+    Icon := diWarning;
+    Title := 'Warning';
+  end;
+
+  // Make a pretty header
+  Summary := Status.Summary;
+
+  if Summary = '' then
+    Summary := 'System error';
+
+  if Assigned(NtxVerboseStatusMessageFormatter) then
+    Content := NtxVerboseStatusMessageFormatter(Status)
+  else
+    Content := NtxVerboseStatusMessageNoReflection(Status);
+end;
+
+function ShowNtxStatus;
+var
+  Icon: TDialogIcon;
+  Title, Summary, Content: String;
+  Response: TMessageResponse;
+begin
+  RtlxpPrepareStatusMessage(Status, Icon, Title, Summary, Content);
+  Result := UsrxShowTaskDialogWithStatus(Response, ParentWnd, Title, Summary,
+    Content, Icon, dbOk, IDOK);
+end;
+
+function ShowNtxStatusAlwaysInteractive;
+var
+  Icon: TDialogIcon;
+  Title, Summary, Content: String;
+  Response: TMessageResponse;
+begin
+  RtlxpPrepareStatusMessage(Status, Icon, Title, Summary, Content);
+  Result := UsrxShowMessageAlwaysInteractiveWithStatus(Response, Title, Summary,
+    Content, Icon, dbOk, IDOK, TimeoutSeconds);
+end;
+
+end.
--- NtUiLib/NtUiLib.Exceptions.Dialog.pas
+++ NtUiLib/NtUiLib.Exceptions.Dialog.pas
@@ -7,7 +7,7 @@ unit NtUiLib.Exceptions.Dialog;
 interface
 
 uses
-  Ntapi.WinUser, System.SysUtils, NtUtils;
+  Ntapi.WinUser, System.SysUtils, NtUtils, NtUiLib.Errors.Dialog;
 
 var
   BUG_TITLE: String = 'This is definitely a bug...';
@@ -21,15 +21,27 @@ type
 // Register a suggestion callback
 procedure RegisterSuggestions(const Callback: TSuggester);
 
-// Show a modal error message to a user
-procedure ShowNtxStatus(ParentWnd: THwnd; const Status: TNtxStatus);
-procedure ShowNtxException(ParentWnd: THwnd; E: Exception);
+// Construct a verbose report about an error
+function NtxVerboseFormatStatusMessage(const Status: TNtxStatus): String;
+
+// Show a modal exception message to a user
+function ShowNtxException(
+  ParentWnd: THwnd;
+  E: Exception
+): TNtxStatus;
+
+// Show an exception message dialog to the interactive user
+function ShowNtxExceptionAlwaysInteractive(
+  E: Exception;
+  TimeoutSeconds: Cardinal = DEFAULT_CROSS_SESSION_MESSAGE_TIMEOUT
+): TNtxStatus;
 
 implementation
 
 uses
-  Ntapi.ntdef, Ntapi.ntstatus, NtUiLib.TaskDialog,
-  NtUiLib.Errors, NtUiLib.Exceptions, NtUiLib.Reflection.Exceptions;
+  Ntapi.ntseapi, Ntapi.ntstatus, DelphiApi.Reflection, NtUtils.SysUtils,
+  NtUiLib.Errors, NtUiLib.TaskDialog, NtUiLib.Exceptions, System.TypInfo,
+  DelphiUiLib.Reflection, DelphiUiLib.Reflection.Strings, System.Rtti;
 
 {$BOOLEVAL OFF}
 {$IFOPT R+}{$DEFINE R+}{$ENDIF}
@@ -67,64 +79,138 @@ begin
     Result := '';
 end;
 
+{ Formatting }
+
+function ProvidesPrivilege(const LastCall: TLastCallInfo): Boolean;
+begin
+  Result := (LastCall.ExpectedPrivilege >= SE_CREATE_TOKEN_PRIVILEGE) and
+    (LastCall.ExpectedPrivilege <= High(TSeWellKnownPrivilege));
+end;
+
+function GetFriendlyName(AType: Pointer): String;
+var
+  RttiContext: TRttiContext;
+  RttiType: TRttiType;
+  a: TCustomAttribute;
+begin
+  RttiContext := TRttiContext.Create;
+  RttiType := RttiContext.GetType(AType);
+
+  for a in RttiType.GetAttributes do
+    if a is FriendlyNameAttribute then
+      Exit(FriendlyNameAttribute(a).Name);
+
+  Result := 'object';
+end;
+
+function NtxVerboseFormatStatusMessage;
+var
+  i: Integer;
+begin
+  // LastCall: <function name>
+  Result := 'Last call: ' + RtlxStringOrDefault(Status.Location, '<unknown>');
+
+  if Status.LastCall.Parameter <> '' then
+    Result := Result +  #$D#$A'Parameter: ' + Status.LastCall.Parameter;
+
+  case Status.LastCall.CallType of
+    lcOpenCall:
+      // Desired access: <mask>
+      Result := Result + #$D#$A'Desired ' +
+        GetFriendlyName(Status.LastCall.AccessMaskType) + ' access: ' +
+        RepresentType(Status.LastCall.AccessMaskType,
+        Status.LastCall.AccessMask).Text;
+
+    lcQuerySetCall:
+      // Information class: <name>
+      Result := Result + #$D#$A'Information class: ' + GetEnumName(
+        Status.LastCall.InfoClassType, Integer(Status.LastCall.InfoClass));
+  end;
+
+  // Expected <type> access: <mask>
+  if Status.Status = STATUS_ACCESS_DENIED then
+    for i := 0 to High(Status.LastCall.ExpectedAccess) do
+      with Status.LastCall.ExpectedAccess[i] do
+        Result := Result + #$D#$A'Expected ' +
+          GetFriendlyName(AccessMaskType) + ' access: ' +
+          RepresentType(AccessMaskType, AccessMask).Text;
+
+  // Result: <STATUS_*/ERROR_*>
+  Result := Result + #$D#$A'Result: ' + Status.Name;
+
+  // <textual description>
+  Result := Result + #$D#$A#$D#$A + Status.Description;
+
+  // <privilege name>
+  if (Status.Status = STATUS_PRIVILEGE_NOT_HELD) and
+    ProvidesPrivilege(Status.LastCall) then
+  begin
+    RtlxSuffixStripString('.', Result, True);
+    Result := Result + ': "' + PrettifySnakeCaseEnum(
+      TypeInfo(TSeWellKnownPrivilege),
+      Integer(Status.LastCall.ExpectedPrivilege), 'SE_') + '"';
+  end;
+end;
+
+procedure RtlxpPrepareExceptionMessage(
+  E: Exception;
+  out Summary: String;
+  out Content: String
+);
+begin
+  Content := E.Message;
+
+  // Include the stack trace when available
+  if Assigned(Exception.GetStackInfoStringProc) then
+    Content := Content + #$D#$A#$D#$A + 'Stack Trace:'#$D#$A + E.StackTrace;
+
+  if (E is EAccessViolation) or (E is EInvalidPointer) or
+    (E is EAssertionFailed) or (E is EArgumentNilException) then
+  begin
+    Content := Content + #$D#$A#$D#$A + BUG_MESSAGE;
+    Summary := BUG_TITLE;
+  end
+  else if E is EConvertError then
+    Summary := 'Conversion error'
+  else
+    Summary := E.ClassName;
+end;
+
 { Showing }
 
-procedure ShowNtxStatus;
+function ShowNtxException;
 var
-  Icon: TDialogIcon;
-  Title, Summary: String;
-begin
-  if Status.IsHResult or NT_ERROR(Status.Status) then
-  begin
-    Icon := diError;
-    Title := 'Error';
-  end
-  else
-  begin
-    Icon := diWarning;
-    Title := 'Warning';
-  end;
-
-  // Make a pretty header
-  Summary := Status.Summary;
-
-  if Summary = '' then
-    Summary := 'System error';
-
-  // TODO: include stack trace when available
-
-  UsrxShowTaskDialog(ParentWnd, Title, Summary,
-    NtxVerboseStatusMessage(Status) + CollectSuggestions(Status), Icon);
-end;
-
-procedure ShowNtxException;
-var
-  Summary, Text: String;
+  Summary, Content: String;
+  Response: TMessageResponse;
 begin
   if E is ENtError then
-    // Extract a TNtxStatus from an exception
-    ShowNtxStatus(ParentWnd, ENtError(E).NtxStatus)
+    // Extract and use TNtxStatus from the exception
+    Result := ShowNtxStatus(ParentWnd, ENtError(E).NtxStatus)
   else
   begin
-    Text := E.Message;
-
-    // Include the stack trace when available
-    if Assigned(Exception.GetStackInfoStringProc) then
-      Text := Text + #$D#$A#$D#$A + 'Stack Trace:'#$D#$A + E.StackTrace;
-
-    if (E is EAccessViolation) or (E is EInvalidPointer) or
-      (E is EAssertionFailed) or (E is EArgumentNilException) then
-    begin
-      Text := Text + #$D#$A#$D#$A + BUG_MESSAGE;
-      Summary := BUG_TITLE;
-    end
-    else if E is EConvertError then
-      Summary := 'Conversion error'
-    else
-      Summary := E.ClassName;
-
-    UsrxShowTaskDialog(ParentWnd, 'Exception', Summary, Text, diError);
+    RtlxpPrepareExceptionMessage(E, Summary, Content);
+    Result := UsrxShowTaskDialogWithStatus(Response, ParentWnd, 'Exception',
+      Summary, Content, diError, dbOk, IDOK);
   end;
 end;
 
+function ShowNtxExceptionAlwaysInteractive;
+var
+  Summary, Content: String;
+  Response: TMessageResponse;
+begin
+  if E is ENtError then
+    // Extract and use TNtxStatus from the exception
+    Result := ShowNtxStatusAlwaysInteractive(ENtError(E).NtxStatus,
+      TimeoutSeconds)
+  else
+  begin
+    RtlxpPrepareExceptionMessage(E, Summary, Content);
+    Result := UsrxShowMessageAlwaysInteractiveWithStatus(Response, 'Exception',
+      Summary, Content, diError, dbOk, IDOK, TimeoutSeconds);
+  end;
+end;
+
+initialization
+  NtxVerboseStatusMessageFormatter := NtxVerboseFormatStatusMessage;
 end.
