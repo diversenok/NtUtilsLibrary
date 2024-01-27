bece6591f1869e67e05af2ad9efca0174ae5b764
diversenok <diversenok@gmail.com>
2024-01-27 14:46:35 +0100

    Add User Manager definitions

---
+++ Headers/Ntapi.umgrapi.pas
@@ -0,0 +1,259 @@
+unit Ntapi.umgrapi;
+
+{
+  This module provides definitions for User Manager service API.
+}
+
+interface
+
+{$WARN SYMBOL_PLATFORM OFF}
+{$MINENUMSIZE 4}
+
+uses
+  Ntapi.WinNt, Ntapi.ntseapi, Ntapi.ProcessThreadsApi, Ntapi.Versions,
+  DelphiApi.DelayLoad, DelphiApi.Reflection;
+
+const
+  usermgrcli = 'usermgrcli.dll';
+
+var
+  delayed_usermgrcli: TDelayedLoadDll = (DllName: usermgrcli);
+
+type
+  // private
+  [SDKName('SESSION_USER_CONTEXT')]
+  TSessionUserContext = record
+    ContextToken: TLuid;
+    SessionId: Cardinal;
+    [Reserved] Reserved: Cardinal;
+  end;
+  PSessionUserContext = ^TSessionUserContext;
+
+  TSessionUserContextArray = TAnysizeArray<TSessionUserContext>;
+  PSessionUserContextArray = ^TSessionUserContextArray;
+
+  // private
+  [SDKName('CRED_PROV_CREDENTIAL')]
+  TCredProvCredential = record
+    [Hex] Flags: Cardinal;
+    AuthenticationPackage: Cardinal;
+    [Counter(ctBytes)] Size: Cardinal;
+    Information: Pointer;
+  end;
+  PCredProvCredential = ^TCredProvCredential;
+
+{ Contexts }
+
+// rev
+[MinOSVersion(OsWin10TH1)]
+procedure UMgrFreeSessionUsers(
+  [in] Buffer: PSessionUserContextArray
+); stdcall; external usermgrcli delayed;
+
+var delayed_UMgrFreeSessionUsers: TDelayedLoadFunction = (
+  DllName: usermgrcli;
+  FunctionName: 'UMgrFreeSessionUsers';
+);
+
+// rev
+[MinOSVersion(OsWin10TH1)]
+[RequiredPrivilege(SE_TCB_PRIVILEGE, rpAlways)]
+function UMgrEnumerateSessionUsers(
+  [out, NumberOfElements] out Count: Cardinal;
+  [out, ReleaseWith('UMgrFreeSessionUsers')] out Contexts:
+    PSessionUserContextArray
+): HResult; stdcall; external usermgrcli delayed;
+
+var delayed_UMgrEnumerateSessionUsers: TDelayedLoadFunction = (
+  DllName: usermgrcli;
+  FunctionName: 'UMgrEnumerateSessionUsers';
+);
+
+// rev
+[MinOSVersion(OsWin10TH1)]
+function UMgrQueryUserContext(
+  [in, Access(TOKEN_QUERY)] TokenHandle: THandle;
+  [out] out ContextToken: TLuid
+): HResult; stdcall; external usermgrcli delayed;
+
+var delayed_UMgrQueryUserContext: TDelayedLoadFunction = (
+  DllName: usermgrcli;
+  FunctionName: 'UMgrQueryUserContext';
+);
+
+// rev
+[MinOSVersion(OsWin10TH1)]
+function UMgrQueryUserContextFromSid(
+  [in] SidString: PWideChar;
+  [out] out ContextToken: TLuid
+): HResult; stdcall; external usermgrcli delayed;
+
+var delayed_UMgrQueryUserContextFromSid: TDelayedLoadFunction = (
+  DllName: usermgrcli;
+  FunctionName: 'UMgrQueryUserContextFromSid';
+);
+
+// rev
+[MinOSVersion(OsWin10TH1)]
+function UMgrQueryUserContextFromName(
+  [in] UserName: PWideChar;
+  [out] out ContextToken: TLuid
+): HResult; stdcall; external usermgrcli delayed;
+
+var delayed_UMgrQueryUserContextFromName: TDelayedLoadFunction = (
+  DllName: usermgrcli;
+  FunctionName: 'UMgrQueryUserContextFromName';
+);
+
+{ Tokens }
+
+// rev
+[MinOSVersion(OsWin10TH1)]
+[RequiredPrivilege(SE_TCB_PRIVILEGE, rpAlways)]
+function UMgrQueryDefaultAccountToken(
+  [out, ReleaseWith('NtClose')] out TokenHandle: THandle
+): HResult; stdcall; external usermgrcli delayed;
+
+var delayed_UMgrQueryDefaultAccountToken: TDelayedLoadFunction = (
+  DllName: usermgrcli;
+  FunctionName: 'UMgrQueryDefaultAccountToken';
+);
+
+// rev
+[MinOSVersion(OsWin10TH1)]
+function UMgrQuerySessionUserToken(
+  [in] SessionId: TSessionId;
+  [out, ReleaseWith('NtClose')] out TokenHandle: THandle
+): HResult; stdcall; external usermgrcli delayed;
+
+var delayed_UMgrQuerySessionUserToken: TDelayedLoadFunction = (
+  DllName: usermgrcli;
+  FunctionName: 'UMgrQuerySessionUserToken';
+);
+
+// rev
+[MinOSVersion(OsWin10TH1)]
+[RequiredPrivilege(SE_TCB_PRIVILEGE, rpForExtendedFunctionality)]
+function UMgrQueryUserToken(
+  [in] Context: TLuid;
+  [out, ReleaseWith('NtClose')] out TokenHandle: THandle
+): HResult; stdcall; external usermgrcli delayed;
+
+var delayed_UMgrQueryUserToken: TDelayedLoadFunction = (
+  DllName: usermgrcli;
+  FunctionName: 'UMgrQueryUserToken';
+);
+
+// rev
+[MinOSVersion(OsWin10TH1)]
+[RequiredPrivilege(SE_TCB_PRIVILEGE, rpForExtendedFunctionality)]
+function UMgrQueryUserTokenFromSid(
+  [in] SidString: PWideChar;
+  [out, ReleaseWith('NtClose')] out TokenHandle: THandle
+): HResult; stdcall; external usermgrcli delayed;
+
+var delayed_UMgrQueryUserTokenFromSid: TDelayedLoadFunction = (
+  DllName: usermgrcli;
+  FunctionName: 'UMgrQueryUserTokenFromSid';
+);
+
+// rev
+[MinOSVersion(OsWin10TH1)]
+[RequiredPrivilege(SE_TCB_PRIVILEGE, rpForExtendedFunctionality)]
+function UMgrQueryUserTokenFromName(
+  [in] UserName: PWideChar;
+  [out, ReleaseWith('NtClose')] out TokenHandle: THandle
+): HResult; stdcall; external usermgrcli delayed;
+
+var delayed_UMgrQueryUserTokenFromName: TDelayedLoadFunction = (
+  DllName: usermgrcli;
+  FunctionName: 'UMgrQueryUserTokenFromName';
+);
+
+// rev
+[MinOSVersion(OsWin10TH1)]
+function UMgrGetConstrainedUserToken(
+  [in, opt] InputTokenHandle: THandle;
+  [in] Context: TLuid;
+  [in, opt] Capabilities: PSecurityCapabilities;
+  [out, ReleaseWith('NtClose'), MayReturnNil] out OutputTokenHandle: THandle
+): HResult; stdcall; external usermgrcli delayed;
+
+var delayed_UMgrGetConstrainedUserToken: TDelayedLoadFunction = (
+  DllName: usermgrcli;
+  FunctionName: 'UMgrGetConstrainedUserToken';
+);
+
+// rev
+[MinOSVersion(OsWin10TH2)]
+[RequiredPrivilege(SE_TCB_PRIVILEGE, rpAlways)]
+function UMgrChangeSessionUserToken(
+  [in] TokenHandle: THandle
+): HResult; stdcall; external usermgrcli delayed;
+
+var delayed_UMgrChangeSessionUserToken: TDelayedLoadFunction = (
+  DllName: usermgrcli;
+  FunctionName: 'UMgrChangeSessionUserToken';
+);
+
+// rev
+[MinOSVersion(OsWin10TH2)]
+[RequiredPrivilege(SE_IMPERSONATE_PRIVILEGE, rpAlways)]
+function UMgrGetImpersonationTokenForContext(
+  [in, Access(TOKEN_QUERY or TOKEN_IMPERSONATE)] InputTokenHandle: THandle;
+  [in] Context: TLuid;
+  [out, ReleaseWith('NtClose')] out OutputTokenHandle: THandle
+): HResult; stdcall; external usermgrcli delayed;
+
+var delayed_UMgrGetImpersonationTokenForContext: TDelayedLoadFunction = (
+  DllName: usermgrcli;
+  FunctionName: 'UMgrGetImpersonationTokenForContext';
+);
+
+// rev
+[MinOSVersion(OsWin10RS1)]
+[RequiredPrivilege(SE_TCB_PRIVILEGE, rpForExtendedFunctionality)]
+function UMgrGetSessionActiveShellUserToken(
+  [in] SessionId: TSessionId;
+  [out, ReleaseWith('NtClose')] out TokenHandle: THandle
+): HResult; stdcall; external usermgrcli delayed;
+
+var delayed_UMgrGetSessionActiveShellUserToken: TDelayedLoadFunction = (
+  DllName: usermgrcli;
+  FunctionName: 'UMgrGetSessionActiveShellUserToken';
+);
+
+{ Credentials }
+
+// rev
+[MinOSVersion(OsWin10TH1)]
+function UMgrFreeUserCredentials(
+  Buffer: PCredProvCredential
+): HResult; stdcall; external usermgrcli delayed;
+
+var delayed_UMgrFreeUserCredentials: TDelayedLoadFunction = (
+  DllName: usermgrcli;
+  FunctionName: 'UMgrFreeUserCredentials';
+);
+
+// rev
+[MinOSVersion(OsWin10TH1)]
+[RequiredPrivilege(SE_TCB_PRIVILEGE, rpAlways)]
+function UMgrGetCachedCredentials(
+  [in] Sid: PSid;
+  [out, ReleaseWith('UMgrFreeUserCredentials')] out Credentials:
+    PCredProvCredential
+): HResult; stdcall; external usermgrcli delayed;
+
+var delayed_UMgrGetCachedCredentials: TDelayedLoadFunction = (
+  DllName: usermgrcli;
+  FunctionName: 'UMgrGetCachedCredentials';
+);
+
+implementation
+
+{$BOOLEVAL OFF}
+{$IFOPT R+}{$DEFINE R+}{$ENDIF}
+{$IFOPT Q+}{$DEFINE Q+}{$ENDIF}
+
+end.
---
+++ NtUtils.UserManager.pas
@@ -0,0 +1,354 @@
+unit NtUtils.UserManager;
+
+{
+  This module provides functions for interacting with User Manager service.
+}
+
+interface
+
+uses
+  Ntapi.WinNt, Ntapi.umgrapi, Ntapi.ntseapi, Ntapi.Versions, NtUtils,
+  DelphiUtils.AutoObjects;
+
+{ Contexts }
+
+// Enumerate session user contexts
+[MinOSVersion(OsWin10TH1)]
+[RequiredPrivilege(SE_TCB_PRIVILEGE, rpAlways)]
+function UmgrxEnumerateSessionUsers(
+  out Contexts: TArray<TSessionUserContext>
+): TNtxStatus;
+
+// Query user context from a token
+[MinOSVersion(OsWin10TH1)]
+function UMgrxQueryUserContext(
+  [Access(TOKEN_QUERY)] hxToken: IHandle;
+  out ContextToken: TLuid
+): TNtxStatus;
+
+// Query user context from a user SID
+[MinOSVersion(OsWin10TH1)]
+function UMgrxQueryUserContextFromSid(
+  const Sid: ISid;
+  out ContextToken: TLuid
+): TNtxStatus;
+
+// Query user context from a user name
+[MinOSVersion(OsWin10TH1)]
+function UMgrxQueryUserContextFromName(
+  const UserName: String;
+  out ContextToken: TLuid
+): TNtxStatus;
+
+{ Tokens }
+
+// Open the token of the default account
+[MinOSVersion(OsWin10TH1)]
+[RequiredPrivilege(SE_TCB_PRIVILEGE, rpAlways)]
+function UMgrxQueryDefaultAccountToken(
+  out hxToken: IHandle
+): TNtxStatus;
+
+// Open the token of the user session
+[MinOSVersion(OsWin10TH1)]
+function UMgrxQuerySessionUserToken(
+  SessionId: TSessionId;
+  out hxToken: IHandle
+): TNtxStatus;
+
+// Open the token of the user by its context
+[MinOSVersion(OsWin10TH1)]
+[RequiredPrivilege(SE_TCB_PRIVILEGE, rpForExtendedFunctionality)]
+function UMgrxQueryUserToken(
+  Context: TLuid;
+  out hxToken: IHandle
+): TNtxStatus;
+
+// Open the token of the user by its SID
+[MinOSVersion(OsWin10TH1)]
+[RequiredPrivilege(SE_TCB_PRIVILEGE, rpForExtendedFunctionality)]
+function UMgrxQueryUserTokenFromSid(
+  const Sid: ISid;
+  out hxToken: IHandle
+): TNtxStatus;
+
+// Open the token of the user by its name
+[MinOSVersion(OsWin10TH1)]
+[RequiredPrivilege(SE_TCB_PRIVILEGE, rpForExtendedFunctionality)]
+function UMgrxQueryUserTokenFromName(
+  const UserName: String;
+  out hxToken: IHandle
+): TNtxStatus;
+
+// Open the impersonation token of the user by its token and context
+[MinOSVersion(OsWin10TH2)]
+[RequiredPrivilege(SE_IMPERSONATE_PRIVILEGE, rpAlways)]
+function UMgrxQueryImpersonationTokenForContext(
+  [Access(TOKEN_QUERY or TOKEN_IMPERSONATE)] hxInputToken: IHandle;
+  Context: TLuid;
+  out hxOutputToken: IHandle
+): TNtxStatus;
+
+// Open the token of the active user shell in the session
+[MinOSVersion(OsWin10RS1)]
+[RequiredPrivilege(SE_TCB_PRIVILEGE, rpForExtendedFunctionality)]
+function UMgrxQueryActiveShellUserToken(
+  SessionId: TSessionId;
+  out hxToken: IHandle
+): TNtxStatus;
+
+implementation
+
+uses
+  Ntapi.ntstatus, Ntapi.ProcessThreadsApi, NtUtils.Ldr, NtUtils.Security.Sid,
+  NtUtils.Tokens, NtUtils.Objects;
+
+{$BOOLEVAL OFF}
+{$IFOPT R+}{$DEFINE R+}{$ENDIF}
+{$IFOPT Q+}{$DEFINE Q+}{$ENDIF}
+
+function UmgrxDelayedFreeSessionUsers(
+  [in] Buffer: Pointer
+): IAutoReleasable;
+begin
+  if not LdrxCheckDelayedImport(delayed_usermgrcli,
+    delayed_UMgrFreeSessionUsers).IsSuccess then
+    Exit(nil);
+
+  Result := Auto.Delay(
+    procedure
+    begin
+      UMgrFreeSessionUsers(Buffer);
+    end
+  );
+end;
+
+[RequiredPrivilege(SE_TCB_PRIVILEGE, rpAlways)]
+function UmgrxEnumerateSessionUsers(
+  out Contexts: TArray<TSessionUserContext>
+): TNtxStatus;
+var
+  Buffer: PSessionUserContextArray;
+  BufferDeallocator: IAutoReleasable;
+  Count: Cardinal;
+  i: Integer;
+begin
+  Result := LdrxCheckDelayedImport(delayed_usermgrcli,
+    delayed_UMgrEnumerateSessionUsers);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  Buffer := nil;
+  Result.Location := 'UMgrEnumerateSessionUsers';
+  Result.LastCall.ExpectedPrivilege := SE_TCB_PRIVILEGE;
+  Result.HResult := UMgrEnumerateSessionUsers(Count, Buffer);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  BufferDeallocator := UmgrxDelayedFreeSessionUsers(Buffer);
+  SetLength(Contexts, Count);
+
+  for i := 0 to High(Contexts) do
+    Contexts[i] := Buffer{$R-}[i]{$IFDEF R+}{$R+}{$ENDIF};
+end;
+
+function UMgrxQueryUserContext;
+begin
+  Result := LdrxCheckDelayedImport(delayed_usermgrcli,
+    delayed_UMgrQueryUserContext);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  Result := NtxExpandToken(hxToken, TOKEN_QUERY);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  Result.Location := 'UMgrQueryUserContext';
+  Result.LastCall.Expects<TTokenAccessMask>(TOKEN_QUERY);
+  Result.HResult := UMgrQueryUserContext(hxToken.Handle, ContextToken);
+end;
+
+function UMgrxQueryUserContextFromSid;
+var
+  SDDL: String;
+begin
+  Result := LdrxCheckDelayedImport(delayed_usermgrcli,
+    delayed_UMgrQueryUserContextFromSid);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  SDDL := RtlxSidToString(Sid);
+  Result.Location := 'UMgrQueryUserContextFromSid';
+  Result.HResult := UMgrQueryUserContextFromSid(PWideChar(SDDL), ContextToken);
+end;
+
+function UMgrxQueryUserContextFromName;
+begin
+  Result := LdrxCheckDelayedImport(delayed_usermgrcli,
+    delayed_UMgrQueryUserContextFromName);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  Result.Location := 'UMgrQueryUserContextFromName';
+  Result.HResult := UMgrQueryUserContextFromName(PWideChar(UserName),
+    ContextToken);
+end;
+
+function UMgrxQueryDefaultAccountToken;
+var
+  hToken: THandle;
+begin
+  Result := LdrxCheckDelayedImport(delayed_usermgrcli,
+    delayed_UMgrQueryDefaultAccountToken);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  hToken := 0;
+  Result.Location := 'UMgrQueryDefaultAccountToken';
+  Result.LastCall.ExpectedPrivilege := SE_TCB_PRIVILEGE;
+  Result.HResult := UMgrQueryDefaultAccountToken(hToken);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  Result := NtxCaptureHandle(hxToken, hToken);
+end;
+
+function UMgrxQuerySessionUserToken;
+var
+  hToken: THandle;
+begin
+  Result := LdrxCheckDelayedImport(delayed_usermgrcli,
+    delayed_UMgrQuerySessionUserToken);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  hToken := 0;
+  Result.Location := 'UMgrQuerySessionUserToken';
+  Result.HResult := UMgrQuerySessionUserToken(SessionId, hToken);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  Result := NtxCaptureHandle(hxToken, hToken);
+end;
+
+function UMgrxQueryUserToken;
+var
+  hToken: THandle;
+begin
+  Result := LdrxCheckDelayedImport(delayed_usermgrcli,
+    delayed_UMgrQueryUserToken);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  hToken := 0;
+  Result.Location := 'UMgrQueryUserToken';
+  Result.HResult := UMgrQueryUserToken(Context, hToken);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  Result := NtxCaptureHandle(hxToken, hToken);
+end;
+
+function UMgrxQueryUserTokenFromSid;
+var
+  hToken: THandle;
+  SDDL: String;
+begin
+  Result := LdrxCheckDelayedImport(delayed_usermgrcli,
+    delayed_UMgrQueryUserTokenFromSid);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  hToken := 0;
+  SDDL := RtlxSidToString(Sid);
+  Result.Location := 'UMgrQueryUserTokenFromSid';
+  Result.HResult := UMgrQueryUserTokenFromSid(PWideChar(SDDL), hToken);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  Result := NtxCaptureHandle(hxToken, hToken);
+end;
+
+function UMgrxQueryUserTokenFromName;
+var
+  hToken: THandle;
+begin
+  Result := LdrxCheckDelayedImport(delayed_usermgrcli,
+    delayed_UMgrQueryUserTokenFromName);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  hToken := 0;
+  Result.Location := 'UMgrQueryUserTokenFromName';
+  Result.HResult := UMgrQueryUserTokenFromName(PWideChar(UserName), hToken);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  Result := NtxCaptureHandle(hxToken, hToken);
+end;
+
+function UMgrxQueryImpersonationTokenForContext;
+var
+  hOutputToken: THandle;
+begin
+  Result := LdrxCheckDelayedImport(delayed_usermgrcli,
+    delayed_UMgrGetImpersonationTokenForContext);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  Result := NtxExpandToken(hxInputToken, TOKEN_QUERY or TOKEN_IMPERSONATE);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  hOutputToken := 0;
+  Result.Location := 'UMgrGetImpersonationTokenForContext';
+  Result.LastCall.Expects<TTokenAccessMask>(TOKEN_QUERY or TOKEN_IMPERSONATE);
+  Result.LastCall.ExpectedPrivilege := SE_IMPERSONATE_PRIVILEGE;
+  Result.HResult := UMgrGetImpersonationTokenForContext(hxInputToken.Handle,
+    Context, hOutputToken);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  Result := NtxCaptureHandle(hxOutputToken, hOutputToken);
+end;
+
+function UMgrxQueryActiveShellUserToken;
+var
+  hToken: THandle;
+begin
+  Result := LdrxCheckDelayedImport(delayed_usermgrcli,
+    delayed_UMgrGetSessionActiveShellUserToken);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  hToken := 0;
+  Result.Location := 'UMgrGetSessionActiveShellUserToken';
+  Result.HResult := UMgrGetSessionActiveShellUserToken(SessionId, hToken);
+
+  if not Result.IsSuccess then
+    Exit;
+
+  Result := NtxCaptureHandle(hxToken, hToken);
+end;
+
+end.
